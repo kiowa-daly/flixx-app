63d1bf78a8fd17dd3f624d3442ffb426e781e09f
kdaly <kdaly@texmtl-uaa.org>
2024-02-12 14:33:16 +0000

    adding init function and highlighting active link

---
+++ flixx-app/js/script.js
@@ -0,0 +1,35 @@
+const globalState = { currentPage: window.location.pathname };
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === globalState.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+//Init app
+function init() {
+  switch (globalState.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movie Details');
+      break;
+    case '/tv-details.html':
+      console.log('TV Details');
+      break;
+    case '/search.html':
+      console.log('Search');
+      break;
+  }
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
